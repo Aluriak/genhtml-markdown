8e0b18415363978f04c70d1d7dbf669d7dae9dd5
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-09-21 16:41:08 +0200

    move tests in their own functions, simplifying their visualization and debug

--- test/test_examples.py
+++ test/test_examples.py
@@ -13,20 +13,22 @@ import genhtml
 from markdown import markdown as markdown_compiler
 
 
-def gen_examples() -> [str, str]:
+def gen_examples() -> [str, str, str]:
     for testfile in glob('examples/*.test'):
         markfile = os.path.splitext(testfile)[0] + '.mkd'
+        testname = os.path.basename(os.path.splitext(testfile)[0])
         assert os.path.exists(markfile)
         with open(markfile) as fd:
             markdown = fd.read()
         with open(testfile) as fd:
             html = fd.read()
-        yield markdown, html
-
-def test_examples():
-    for markdown, expected_html in gen_examples():
-        html = markdown_compiler(markdown, extensions=['genhtml'])
-        assert html.rstrip() == expected_html.rstrip()  # handle (un)voluntary eol at the eof
+        yield testname, markdown, html
 
 
+def template_test_example(markdown:str, expected_html:str):
+    html = markdown_compiler(markdown, extensions=['genhtml'])
+    assert html.rstrip() == expected_html.rstrip()  # handle (un)voluntary eol at the eof
 
+
+for testname, markdown, expected_html in gen_examples():
+    globals()['test_' + testname] = lambda: template_test_example(markdown, expected_html)
