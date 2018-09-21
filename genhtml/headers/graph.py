cc421e21e2f7006d33278b9b123457db0c91d4b2
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-09-21 17:25:21 +0200

    graph related imports

---
+++ genhtml/headers/graph.py
@@ -0,0 +1,21 @@
+# graph-related packages
+try:
+    import pydot
+except ImportError:
+    pass
+try:
+    import networkx as nx
+except ImportError:
+    pass
+try:
+    import powergrasp
+except ImportError:
+    pass
+try:
+    import bubbletools
+except ImportError:
+    pass
+try:
+    import phasme
+except ImportError:
+    pass
